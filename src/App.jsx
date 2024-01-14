5aac43fcdab2b10556684436a77e3c5b89293d52
ervinsv92 <ervinsv92@gmail.com>
2024-01-13 18:46:27 -0600

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useCounterStore} from './store/counterStore';
+import {shallow} from 'zustand/shallow';
 
 const App = () => {
+
+  const {count, name} = useCounterStore(state => ({
+    count:state.count,
+    name:state.name
+  }), shallow); //Shallow sirve para cuando se ocupa detectar que algo dentro de un objeto cambio
+
+  //const increment = useCounterStore(state => state.increment);
+  //const count = useCounterStore(state => state.count);
+  //const name = useCounterStore(state => state.name);
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+  
+
+  const {increment, getPosts, posts, multiply} = useCounterStore();
+  
   return (
-    <div>App</div>
+    <div>
+      <p>{count}</p>
+      <p>{name}</p>
+      <button onClick={() => increment(3)}>+3</button>
+      <button onClick={() => multiply(2)}>x2</button>
+      <p>{JSON.stringify(posts)}</p>
+    </div>
   )
 }
 
--- src/store/counterStore.js
+++ src/store/counterStore.js
@@ -1,5 +1,27 @@
 import { create } from "zustand";
 
-export const useCounterStore = create(() => ({
-    count:10
+//el set y get son propios de zustand
+export const useCounterStore = create((set,get) => ({
+    count:10,
+    name:'Hola',
+    posts:[],
+    increment: (value) => set(state => ({
+        count:state.count + value
+    })),
+    getPosts: async () => {
+        const posts = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+        //const posts = await res.json()
+        //console.log(posts)
+        set(state => ({
+            ...state,
+            posts
+        }))
+    },
+    clearStore: ()=>{
+        set({}, true) //limpia todo el estado hasta las funciones
+    },
+    multiply:(value) => {
+        const {count} = get()
+        set({count: count * value})
+    }
 }));
